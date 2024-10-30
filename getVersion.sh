7e1b10e813d1e6cf987bb852da770ab844608e4c
cyfang666 <fcy970106@163.com>
2024-10-30 17:50:43 +0800

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,1 +1,1 @@
-kubectl rollout history deploy/jenkins-demo -n test-1 | egrep -v "deployment|REVISION" | awk '{print $1}' > version.csv
+kubectl rollout history deploy/jenkins-demo -n <namespace> | egrep -v "deployment|REVISION" | awk '{print $1}' > version.csv
